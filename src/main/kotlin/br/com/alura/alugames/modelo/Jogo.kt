9369e996434763d1dcb85f7bf26e3103cb6f69fe
Eiti <eric.nomura33@gmail.com>
2024-08-19 21:09:40 -0300

    feat: Trabalhar com escopos de variaveis, tratamento de excecoes com try-catch e runCatching, Null Safety e boas praticas de organizacao de pacotes no projeto em Kotlin

--- src/main/kotlin/br/com/alura/alugames/modelo/Jogo.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Jogo.kt
@@ -1,11 +1,15 @@
+package br.com.alura.alugames.modelo
+
 data class Jogo(val titulo:String,
-           val capa:String) {
-    val descicao = ""
+                val capa:String) {
+    var descricao: String? = null
     override fun toString(): String {
         return "Meu Jogo: \n" +
                 "Título: $titulo \n" +
                 "Capa: $capa \n" +
-                "Descrição: $descicao"
+                "Descricao: $descricao"
+
     }
+
 
 }
